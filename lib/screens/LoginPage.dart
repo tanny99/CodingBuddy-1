99907502eca44f124cfc954c0a203c309735e1a7
Astroakanksha <62433778+Astroakanksha24@users.noreply.github.com>
2022-02-15 20:02:55 +0530

    login

--- lib/screens/LoginPage.dart
+++ lib/screens/LoginPage.dart
@@ -5,6 +5,8 @@ import 'package:jiit_hub/screens/HomePage.dart';
 import 'package:jiit_hub/screens/SignupPage.dart';
 import 'package:jiit_hub/responsive_constants.dart';
 import 'package:jiit_hub/screens/Methods.dart';
+import 'package:jiit_hub/styles/app_text_style.dart';
+import 'package:jiit_hub/styles/app_color.dart';
 import '../Constants.dart' as K;
 
 class LoginPage extends StatefulWidget {
@@ -13,183 +15,257 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final TextEditingController EnrollmentController=TextEditingController();
-  final TextEditingController PasswordController=TextEditingController();
+  final TextEditingController EnrollmentController = TextEditingController();
+  final TextEditingController PasswordController = TextEditingController();
+
+  bool _isSecure = true;
   bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-            child: Padding(
-              padding: const EdgeInsets.only(right: 55.0),
-              child: Text(
-                  "JIIT HUB",
-              ),
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [AppColor.primaryLight, AppColor.primary]),
         ),
-          backgroundColor: Color.fromRGBO(29, 53, 87, 1),
-      ),
-      backgroundColor: Color.fromRGBO(231, 227, 238, 1),
-
-      body: isLoading? Center(
-        child: Container(
-          child: CircularProgressIndicator(),
-        ),
-      )
-          : SingleChildScrollView(
-            child: Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Colors.white,
-                    Colors.lightBlueAccent,
-                    Color.fromRGBO(29, 53, 87, 1),
-                  ],
-                )
-            ),
-              child: Column(
-                children: [
-                Padding(
-                  padding: const EdgeInsets.only(top:20),
-                  child: Container(
-                    child: Image(image: AssetImage('Assets/new.jpg'),height: Responsive.height(30, context),),
-                  ),
-                ),
-                SizedBox(
-                  height: Responsive.height(1, context),
-                ),
+        child: Column(
+          children: [
+            Stack(
+              alignment: Alignment.bottomLeft,
+              children: [
                 Container(
-                  child: Text('Login',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 33,
-                        fontWeight: FontWeight.bold
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  child: Image.asset("Assets/splash.png"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Hello Buddy",
+                        style: AppTextStyle.style(
+                            fontSize: 34, fontWeight: FontWeight.w600),
+                      ),
+                      Text(
+                        "login to Continue",
+                        style: AppTextStyle.style(
+                            fontSize: 20,
+                            color: Colors.white.withOpacity(0.4),
+                            fontWeight: FontWeight.w300),
+                      )
+                    ],
                   ),
-                      textAlign: TextAlign.center),
+                )
+              ],
+            ),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(38),
+                    topLeft: Radius.circular(38),
+                  ),
                 ),
-                SizedBox(
-                  height: Responsive.height(2, context),
-                ),
-                Row(
-                  children:[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Icon(CupertinoIcons.mail),
-                    ),
-                    Container(
-                      width: Responsive.width(80, context),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: "Email Address",
-                          hintStyle: TextStyle(color: Colors.white)
-                        ),
-                        keyboardType: TextInputType.text,
-                        textAlign: TextAlign.left,
-                        controller: EnrollmentController,
-                      ),
-                    ),
-                  ]
-                ),
-                SizedBox(
-                  height: Responsive.height(2, context),
-                ),
-                Row(
-                  children:[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Icon(CupertinoIcons.lock),
-                    ),
-                    Container(
-                      width: Responsive.width(80, context),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: "Password",
-                          hintStyle: TextStyle(color: Colors.white)
-                        ),
-                        obscureText: true,
-                        keyboardType: TextInputType.text,
-                        textAlign: TextAlign.left,
-                        controller: PasswordController,
-                      ),
-                    ),
-                  ]
-                ),
-                SizedBox(
-                  height: Responsive.height(5, context),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                child: Column(
                   children: [
-                    Container(
-                      width: Responsive.width(50, context),
-                      height: 50,
-                      child: ElevatedButton(
-                        style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Color.fromRGBO(29, 53, 87, 1))),
-                        child: Text(
-                          'Login',
-                          style: TextStyle(fontSize: 20,),
-                        ),
-                        onPressed: ()async {
-                          if(EnrollmentController.text.isNotEmpty &&
-                            PasswordController.text.isNotEmpty) {
-                              setState(() {
-                                isLoading = true;
-                              });
-                              LogIn(EnrollmentController.text, PasswordController.text)
-                              .then((user) {
-                                if(user != null) {
-                                  print("Login Successful");
-                                  setState(() {
-                                    isLoading = false;
-                                  });
-                                  Navigator.push(context, MaterialPageRoute(builder: (_) => HomePage(indexx: 0,)));
-                                } else{
-                                  print("Login failed");
-                                  setState(() {
-                                    isLoading = false;
-                                  });
-                                }
-                              });
-                          } else{
-                            print("Please fill the form correctly");
-                          }
-                        },
+                    SizedBox(height: 20),
+                    TextField(
+                      style: AppTextStyle.style(
+                        color: Colors.black.withOpacity(0.8),
                       ),
+                      decoration: InputDecoration(
+                        hintText: 'Hipporasy@gmail.com',
+                        labelText: 'Email',
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black.withOpacity(0.8)),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black.withOpacity(0.8)),
+                        ),
+                        border: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black.withOpacity(0.8)),
+                        ),
+                        labelStyle: AppTextStyle.style(
+                          color: Colors.black.withOpacity(0.8),
+                        ),
+                      ),
+                      controller: EnrollmentController,
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      obscureText: _isSecure,
+                      style: AppTextStyle.style(
+                        color: Colors.black.withOpacity(0.8),
+                      ),
+                      decoration: InputDecoration(
+                        suffixIcon: Padding(
+                          padding: EdgeInsets.all(15),
+                          child: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                _isSecure = !_isSecure;
+                              });
+                            },
+                            child: Icon(
+                              Icons.remove_red_eye,
+                              color: _isSecure
+                                  ? Colors.black.withOpacity(0.8)
+                                  : Colors.black.withOpacity(0.4),
+                            ),
+                          ),
+                        ),
+                        labelText: 'Password',
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black.withOpacity(0.8)),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black.withOpacity(0.8)),
+                        ),
+                        border: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black.withOpacity(0.8)),
+                        ),
+                        labelStyle: AppTextStyle.style(
+                          color: Colors.black.withOpacity(0.8),
+                        ),
+                      ),
+                      controller: PasswordController,
+                    ),
+                    SizedBox(height: 32),
+                    Container(
+                      height: 56,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            AppColor.primary,
+                            AppColor.primary.withOpacity(0.7)
+                          ],
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            offset: Offset(5, 5),
+                            blurRadius: 10,
+                          )
+                        ],
+                      ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(4),
+                                child: SizedBox(
+                                  width: double.infinity,
+                                  child: ElevatedButton(
+                                    style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.all(AppColor
+                                              .primary
+                                              .withOpacity(0.7)),
+                                    ),
+                                    child: Text(
+                                      'SIGN IN',
+                                      style: AppTextStyle.style(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                    onPressed: () async {
+                                      if (EnrollmentController
+                                              .text.isNotEmpty &&
+                                          PasswordController.text.isNotEmpty) {
+                                        setState(() {
+                                          isLoading = true;
+                                        });
+                                        LogIn(EnrollmentController.text,
+                                                PasswordController.text)
+                                            .then((user) {
+                                          if (user != null) {
+                                            print("Login Successful");
+                                            setState(() {
+                                              isLoading = false;
+                                            });
+                                            Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (_) => HomePage(
+                                                          indexx: 0,
+                                                        )));
+                                          } else {
+                                            print("Login failed");
+                                            setState(() {
+                                              isLoading = false;
+                                            });
+                                          }
+                                        });
+                                      } else {
+                                        print("Please fill the form correctly");
+                                      }
+                                    },
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          // Padding(
+                          //   padding: const EdgeInsets.only(right: 20),
+                          //   child: Icon(Icons.arrow_forward,
+                          //       color: Colors.white, size: 32),
+                          // )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (_) => CreateAccount()));
+                          },
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.only(top: 15.0, right: 50),
+                            child: Text(
+                              'Not registered?',
+                              style: AppTextStyle.style(
+                                color: Colors.black.withOpacity(0.8),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: Responsive.height(9.7, context),
                     ),
                   ],
                 ),
-                SizedBox(
-                  height: Responsive.height(2, context),
-                ),
-                Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(
-                    onPressed: (){
-                       Navigator.push(context, MaterialPageRoute(builder: (_)=> CreateAccount()));
-                    },
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 15.0,right: 50),
-                        child: Text(
-                            'Not registered?',
-                          style: TextStyle(fontSize: 20,color:Colors.white),
-                        ),
-                      ),
-                    ),
-                ],
-                ),
-                SizedBox(
-                  height: Responsive.height(9.7, context),
-                ),
-             ],
-            ),
-            ),
-          ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/styles/app_color.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+mixin AppColor {
+  static Color primary = Color(0xFF2855AE);
+  static Color primaryLight = Color(0xFF7292CF);
+  static Color darkText = Color(0xFF777777);
+}
