6a28afe5b8da1179e61c356bff23f3426e1f90de
Astroakanksha24 <62433778+Astroakanksha24@users.noreply.github.com>
2022-02-17 01:22:13 +0530

    search

--- lib/screens/MyProfile.dart
+++ lib/screens/MyProfile.dart
@@ -1,106 +1,41 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:jiit_hub/screens/Methods.dart';
 import 'package:jiit_hub/responsive_constants.dart';
-import 'package:jiit_hub/styles/app_color.dart';
-import 'package:jiit_hub/styles/app_text_style.dart';
 
-import 'package:jiit_hub/screens/HomePage.dart';
-import 'SignupPage.dart';
-
-class MyProfile extends StatefulWidget {
-  @override
-  State<MyProfile> createState() => _MyProfileState();
-}
-
-class _MyProfileState extends State<MyProfile> {
-  final TextEditingController PasswordController = TextEditingController();
-  final TextEditingController EnrollmentController = TextEditingController();
-  // final TextEditingController DeptController = TextEditingController();
-
-  bool _isSecure = true;
-  bool isLoading = false;
+class MyProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [AppColor.primaryLight, AppColor.primary]),
-        ),
-        child: Column(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
           children: [
-            SingleChildScrollView(
-              child: Column(
-                children: [
-                  Column(
-                    children: [
-                      SizedBox(height: 50),
-                      Container(
-                        height: 56,
-                        padding: EdgeInsets.all(12.0),
-                        margin: EdgeInsets.all(10.0),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          border: Border.all(
-                            color: Colors.white70,
-                            width: 2,
-                          ),
-                          gradient: LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [
-                              AppColor.primary,
-                              AppColor.primary.withOpacity(0.7)
-                            ],
-                          ),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black12,
-                              offset: Offset(5, 5),
-                              blurRadius: 10,
-                            )
-                          ],
-                        ),
-                        child: TextField(
-                          style: AppTextStyle.style(
-                            color: Colors.white.withOpacity(0.8),
-                          ),
-                          decoration: InputDecoration(
-                            hintText: 'College name',
-                            labelText: 'College',
-                            focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Colors.white.withOpacity(0.8)),
-                            ),
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Colors.white.withOpacity(0.8)),
-                            ),
-                            border: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Colors.white.withOpacity(0.8)),
-                            ),
-                            labelStyle: AppTextStyle.style(
-                              color: Colors.white.withOpacity(0.8),
-                            ),
-                          ),
-                          controller: EnrollmentController,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      SizedBox(
-                        height: Responsive.height(9.7, context),
-                      ),
-                    ],
-                  ),
-                ],
+            Container(
+              child: Text('College'),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: Container(
+                width: Responsive.width(80, context),
+                child: TextField(
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration:
+                      InputDecoration(labelText: ' ', hintText: 'Tanishk'),
+                ),
               ),
-            )
+            ),
+            Container(
+              child: Text('Year'),
+            ),
+            Container(
+              child: Text('Department'),
+            ),
+            Container(
+              child: Text('Class'),
+            ),
+            Container(
+              child: Text('Invite'),
+            ),
           ],
         ),
       ),
